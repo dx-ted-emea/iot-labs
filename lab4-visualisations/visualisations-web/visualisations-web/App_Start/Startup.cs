5f950b3a3521580d99956747136f29689f9cc0c7
Andy Cross <andy@elastacloud.com>
2014-12-04 13:07:31 +0000

    scaffold

---
+++ lab4-visualisations/visualisations-web/visualisations-web/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(visualisations_web.App_Start.Startup))]
+
+namespace visualisations_web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
