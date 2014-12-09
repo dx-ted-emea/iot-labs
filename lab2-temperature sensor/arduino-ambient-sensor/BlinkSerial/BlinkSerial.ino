0498fb0e2625e0acd36128e692d33a8c95e008b3
Andy Cross <andy@elastacloud.com>
2014-12-09 16:36:38 +0000

    work on lab 3 and 4

--- lab2-temperature sensor/arduino-ambient-sensor/BlinkSerial/BlinkSerial.ino
+++ lab2-temperature sensor/arduino-ambient-sensor/BlinkSerial/BlinkSerial.ino
@@ -1,50 +1,70 @@
-/*
-  based on Blink, Arduino's "Hello World!"
-  Turns on an LED on for one second, then off for one second, repeatedly.
-  The Tinkerkit Led Modules (T010110-7) is hooked up on O0
- 
- 
-  This example code is in the public domain.
- */
- 
-#define O0 11
-#define O1 10
-#define O2 9
-#define O3 6
-#define O4 5
-#define O5 3
-#define I0 A0
-#define I1 A1
-#define I2 A2
-#define I3 A3
-#define I4 A4
-#define I5 A5
  
 int incomingByte = 0;   // for incoming serial data
+boolean poweredOn = false;
  
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(O3, OUTPUT);      
+  pinMode(13, OUTPUT);      
   Serial.begin(9600);  //Start the serial connection with the computer
                        //to view the result open the serial monitor 
                        
   Serial.setTimeout(10000);
+  
+  // cycle LED to prove this electrically circuited
+  flash(1000);
 }
- 
-void loop() {
-  digitalWrite(O3, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(O3, LOW);    // set the LED off
-  delay(1000);              // wait for a second
-  
-   if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
 
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
-        }
+void flash(int duration) {
+    // cycle LED to prove this electrically circuited
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(duration);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(duration);  
+}
+
+
+ 
+void loop() { 
+  // wait for a second
+  // read the incoming byte:
   
+      String content = "";
+      char serialdata[80];
+      int nchars;
+    
+      nchars=Serial.readBytesUntil('\0', serialdata, 80);
+      content = String(serialdata);
+      content = content.substring(0, nchars);
+      content.trim();
+      
+      if (content != "") {  
+        if (content == "ON") 
+        {
+          digitalWrite(13, HIGH);
+          poweredOn = true;
+            Serial.print("ON");
+        }
+        else if (content == "OFF")
+        {
+          digitalWrite(13, LOW);
+          poweredOn = false;
+            Serial.print("OFF");
+        }
+        else if (content == "QUERY")
+        {
+          if (poweredOn) 
+          {
+            Serial.print("ON");
+          }
+          else 
+          {
+            Serial.print("OFF");
+          }                  
+        }
+        else
+        {
+          Serial.print("ERR: " + content);
+        }
+      }
 }
