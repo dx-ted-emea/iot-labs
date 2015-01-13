a107dedafd1fce9860d6a7ce69f956f5b63d9c89
david@elastacloud.com <david@elastacloud.com>
2015-01-13 10:55:18 +0000

    removed sql connection fixed unit test

--- lab2-energy monitor/src/storm/src/test/java/com/hackathon/storm/AppTest.java
+++ lab2-energy monitor/src/storm/src/test/java/com/hackathon/storm/AppTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.mock;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
     /**
@@ -53,75 +53,38 @@ public class AppTest
 
         MockOutputCollector out = new MockOutputCollector();
         MockBasicOutputCollector mockoc = new MockBasicOutputCollector(out);
-       BasicOutputCollector collector = new BasicOutputCollector(mockoc);
+        BasicOutputCollector collector = new BasicOutputCollector(mockoc);
 
         TopologyContext context = mock(TopologyContext.class);
         Map conf = mock(Map.class);
 
-        IBasicBolt bolt = new RedisStorageBolt();
+        IBasicBolt bolt = new ParseBolt();
         Map<String, String> field1 = new HashMap<String, String>();
-
 
         MkTupleParam param = new MkTupleParam();
         param.setStream("test-stream");
         param.setComponent("test-component");
 
-        param.setFields("timestamp", "deviceid", "startReading", "endReading", "energyUsage", "servertimestamp");
+        param.setFields("string");
 
         Tuple tuple = null;
-        int endReading = 5000;
-        int startReading = 5000;
-
 
         Calendar cal = new GregorianCalendar();
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
 
-        for(int i=0; i < 24* 60*60; i++)
-        {
-            int plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        String time = sdf.format(cal.getTime());
 
-            Random r = new Random();
-            int amount = r.nextInt(100);
-            startReading = endReading;
-            endReading += 1000+(plusOrMinus * amount);
-            int energyUsage = endReading - startReading;
+        String str = String.format("{ \"deviceid\" : \"device0\", \"timestamp\" : \"%s\", \"startReading\" : %d, \"endReading\" : %d, \"energyUsage\" : %d }", time.toString(), 5000, 5080, 80);
 
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-            String time = sdf.format(cal.getTime());
+        tuple = Testing.testTuple(new Values(str), param);
+        bolt.prepare(conf, context);
+        bolt.execute(tuple,(BasicOutputCollector)collector);
 
-            tuple = Testing.testTuple(new Values(time, "Device01", startReading, endReading, energyUsage, "2014-12-04T12:10:46.394Z"), param);
-            bolt.prepare(conf, context);
-            bolt.execute(tuple,(BasicOutputCollector)collector);
+        List<Values> result = mockoc.getEmittedValues();
 
-            cal.add(Calendar.SECOND, 1);
-        }
-
-
-
-
-
-
-
-
-
-        /*Gson gson = new Gson();
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("deviceId", "a");
-        obj.put("timestamp", 5);
-        obj.put("reading ", "c");
-        obj.put("servertimestamp", "sdf");
-        System.out.println(gson.toJson(obj));*/
-
-
-       /* DateFormat targetFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
-        String servertimestamp = targetFormat.format(new Date());
-        System.out.println(servertimestamp);
-        */
-/*        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
-        Date dateStr = formatter.parse("2014-12-04T12:10:46.394Z");*/
-
- /*       System.out.format("%02d",12);*/
+        assert(result.size() == 1 && result.get(0).size() == 5);
     }
 }
