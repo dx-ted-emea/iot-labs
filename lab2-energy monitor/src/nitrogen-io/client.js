34f1c788e1071cbff863893f7bf44e116a7d0cf1
david@elastacloud.com <david@elastacloud.com>
2015-01-13 15:32:56 +0000

    added additional fields to the json object

--- lab2-energy monitor/src/nitrogen-io/client.js
+++ lab2-energy monitor/src/nitrogen-io/client.js
@@ -13,16 +13,23 @@ client.on('connect', function () {
     while(true)
     {
         baseReading += Math.floor( Math.random() * ( 1 + 10 - 0 ) ) + 0
+        var endReading = baseReading + Math.floor( Math.random() * ( 1 + 100 - 0 ) ) + 0
+        var energyUsage = endReading - baseReading;
+
         var deviceID = Math.floor( Math.random() * ( 1 + 3 - 0 ) ) + 0
         var now = new Date()
         var obj = {timestamp: now,
             deviceId : "DeviceId" + deviceID,
-            reading : baseReading};
+            startReading : baseReading,
+            endReading : endReading,
+            energyUsage : energyUsage};
 
         var payload = JSON.stringify(obj)
 
         var result = client.publish('messages', payload );
-        console.log(result)
+        console.log(payload)
+
+        baseReading = endReading;
     }
 });
 
